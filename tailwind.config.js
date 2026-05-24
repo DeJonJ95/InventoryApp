684f0627ea4c3ae997fd761ce8459db2239cca84
DeJon Johnson <dejon.johnson@detroitmi.gov>
2026-05-24 11:51:59 -0400

    Switch to Inter font for minimal/professional typography

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,9 @@ module.exports = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ["'Inter'", 'ui-sans-serif', 'system-ui', '-apple-system', 'sans-serif'],
+      },
       colors: {
         brand: {
           darkest: '#212529',
